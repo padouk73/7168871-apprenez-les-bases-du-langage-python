72736a46890394d13f52090a45861fb34aa92a3e
padouk73 <60747416+padouk73@users.noreply.github.com>
2026-02-18 18:44:38 +0100

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,14 @@
-# Écrivez votre code ici !
+nom = "bibi"
+age =35
+taille = 1.85
+est_etudiant = False
+
+print (f"je m'appelle {nom}")
+print (f"j'ai {age} ans")
+print (f"ma taille est {taille=}")
+print (f"suis-je étudiant ? {est_etudiant}")
+
+print (f"type de la variable nom {type(nom)}")
+print (f"type de la variable age {type(age)}")
+print (f"type de la variable taille {type(taille)}")
+print (f"type de la variable est_etudiant {type(est_etudiant)}")
