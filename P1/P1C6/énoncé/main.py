0305c3c6f3da44fe63dbf71b5a6f47ab953de745
padouk73 <60747416+padouk73@users.noreply.github.com>
2026-02-19 10:04:11 +0100

    Update main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,7 @@
-# Écrivez votre code ici !
+fruits = ["pomme","banane","orange"]
+fruits.append("kiwi")
+fruits.remove("orange")
+fruits[1]=("ananas")
+print(len(fruits))
+fruits.sort()
+print(fruits)
